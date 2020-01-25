1c32863fbd531cfd2580ed54c743c72d6c1a7114
Mihael Anđel <mandel@foi.hr>
2020-01-25 12:26:24 +0100

    dodani komentari i maknut nepotreban import

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from itertools import groupby
 import heapq
 
-kodovi = {}
-kodoviObrnuto = {}
-putanjaOriginal = r'./datoteke/tekst.txt'
-putanjaKompresija = r'./datoteke/kompresija.bin'
-putanjaDekompresija = r'./datoteke/dekompresija.txt'
-frekvencijePojavljivanja = {}
-cvorovi = []
+
+kodovi = {} # Dictionary s odgovarajućim kodovima za svaki znak u tekstu
+kodoviObrnuto = {} # Dictionary koji služi za dekodiranje iz kodova u znakove 
+putanjaOriginal = r'./datoteke/tekst.txt'  # Putanja do datoteke s originalnim tekstom za kodiranje/dekodiranje
+putanjaKompresija = r'./datoteke/kompresija.bin' # Putanja do datoteke s kodiranim tekstom
+putanjaDekompresija = r'./datoteke/dekompresija.txt' # Putanja do datoteke s dekodiranim tekstom
+frekvencijePojavljivanja = {} # Dictionary s frekvencijama pojavljivanja svakog znaka u tekstu
+cvorovi = [] # Lista svih cvorova u stablu
 
 class Cvor(object):
   lijevoDijete = None
   desnoDijete = None
   znak = None
-  vrijednost = 0
+  vrijednost = 0 # Frekvencija pojavljivanja znaka, odnosno zbroj frekvencija pojavljivanja u roditelju
 
   def __init__(self, znak, vrijednost):
     self.znak = znak
     self.vrijednost = vrijednost
 
+  # Metode za usporedbu cvorova
   def __gt__(self, c):
     return self.vrijednost > c.vrijednost
 
@@ -39,6 +40,8 @@ def zapisiTekstUDatoteku(putanja, tekst):
   datoteka.close()
 
 def popuniFrekvencijePojavljivanja(tekst):
+  # Prolazi kroz svaki znak teksta i inkrementira 
+  # odgovarajući zapis u dictionaryu
   for znak in tekst:
     if not znak in frekvencijePojavljivanja:
       frekvencijePojavljivanja[znak] = 0
@@ -46,11 +49,14 @@ def popuniFrekvencijePojavljivanja(tekst):
     frekvencijePojavljivanja[znak] += 1
 
 def stvoriCvorove():
+  # Puni listu cvorova tako da lista predstavlja hrpu cvorova
   for znak in frekvencijePojavljivanja:
     cvor = Cvor(znak, frekvencijePojavljivanja[znak])
     heapq.heappush(cvorovi, cvor)
   
-def spojiCvorove(): 
+def spojiCvorove():
+  # Rekurzivna funkcija koja "spaja" cvorove,
+  # odnosno konstruira stablo na temelju liste cvorova
   while len(cvorovi) > 1:
     lijevi = heapq.heappop(cvorovi)
     desni = heapq.heappop(cvorovi)
@@ -62,10 +68,14 @@ def spojiCvorove():
     heapq.heappush(cvorovi, roditelj)
 
 def izradiKodove(s, cvor):
-  if cvor.znak:
+  # Rekurzivna funkcija koja puni dictionary
+  # s kodovima za svaki znak u tekstu
+
+  if cvor.znak: # Ako je cvor list (ima znak vezan uz sebe)
     if not s:
       kodovi[cvor.znak] = '0'
     else:
+      # Dodijeli cvoru njegov Huffman kod
       kodovi[cvor.znak] = s
       kodoviObrnuto[s] = cvor.znak
   else:
@@ -73,6 +83,8 @@ def izradiKodove(s, cvor):
     izradiKodove(s + '1', cvor.desnoDijete)
 
 def kodirajTekst(tekst):
+  # Funkcija koja generira string kodiranih
+  # znakova originalnog teksta
   kodiraniTekst = ''
 
   for znak in tekst:
@@ -81,6 +93,9 @@ def kodirajTekst(tekst):
   return kodiraniTekst
 
 def dodajPadding(tekst):
+  # Dodaje padding i formatira string
+  # s kodovima znakova kako bi se korektno
+  # mogli zapisatu u binarnom obliku u datoteku
   paddingExtra = 8 - len(tekst) % 8
   for i in range(paddingExtra):
     tekst += '0'
@@ -90,6 +105,8 @@ def dodajPadding(tekst):
   return tekst
 
 def stvoriKodiraneBajtove(kodiraniTekst):
+  # Prima kodirani tekst s paddingom i pretvara
+  # ga u niz bajtova
   bajtovi = bytearray()
 
   for i in range(0, len(kodiraniTekst), 8):
@@ -109,15 +126,18 @@ def ucitajKodiraneBajtove():
 
   bajt = komprimiranaDatoteka.read(1)
   while (bajt != b''):
+    # Radi toliko dugo dok ne dođe do kraja datoteke,
+    # odnosno dok ne pročita bitove za prazan string
     bajt = ord(bajt)
     bits = bin(bajt)[2:].rjust(8, '0')
-    bitovi += bits
+    bitovi += bits # Dodaje pročitane bitove u string koji se kasnije dekodira
     bajt = komprimiranaDatoteka.read(1)
 
   komprimiranaDatoteka.close()
   return bitovi
 
 def makniPadding(tekst):
+  # Funkcija miče prije dodan padding iz očitanog niza bitova
   paddingInfo = tekst[:8]
   paddingExtra = int(paddingInfo, 2)
 
@@ -130,6 +150,9 @@ def dekodirajTekst():
   kod = ''
   dekodiraniTekst = ''
   for bit in kodiraniTekst:
+    # Prolazi znak po znak kroz kodirani tekst. Dok nađe
+    # niz koji odgovara nekom kodu u dictionaryu, stavlja odgovarajući
+    # znak u string koji predstavlja dekodirani tekst.
     kod += bit
     if (kod in kodoviObrnuto):
       znak = kodoviObrnuto[kod]
